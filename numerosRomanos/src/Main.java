436b73322b57b89d32aa54618db442d4d2b5f956
tats <112152266+bozotats@users.noreply.github.com>
2024-08-30 16:25:13 +0000

    CSV out y entrada en CLI

--- numerosRomanos/src/Main.java
+++ numerosRomanos/src/Main.java
@@ -1,13 +1,37 @@
+import java.io.File;
+import java.io.IOException;
+import java.io.FileWriter;
+
 public class Main {
     public static void main(String[] args) {
         numeracion numeracion = new numeracion();
         //Entrada de datos
-        String[] numero = {"XIII", "XXX", "IX", "LVIII", "MCMXCIV"};
+        String[] numero = args;
         int[] resultados = new int[numero.length];
+        
 
-        for (int i = 0; i < numero.length; i++) {
-            resultados[i] = numeracion.valores(numero[i]);
-            System.out.println("El valor de " + numero[i] + " es: " + resultados[i]);
+        try {
+            File salidaCSV = new File("outNumRomanos.csv");
+            if (!salidaCSV.createNewFile()) {
+                System.out.println("el archivo de salida ya existe, sera sobreescrito");
+            }
+        } catch(IOException e) {
+            System.out.println("Ha sucedido un error");
+            e.printStackTrace();
         }
+
+        try {
+            FileWriter escritorCSV = new FileWriter("outNumRomanos.csv");
+            for (int i = 0; i < numero.length; i++) {
+                resultados[i] = numeracion.valores(numero[i]);
+                System.out.println("El valor de " + numero[i] + " es: " + resultados[i]);
+                escritorCSV.write(numero[i] + "," + resultados[i]);
+            }
+            escritorCSV.close();
+        } catch(IOException e) {
+            System.out.println("Ha sucedido un error");
+            e.printStackTrace();
+        }
+        
     }
 }
